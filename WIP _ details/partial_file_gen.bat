42c93247647531af629f868c573cf5d84a9959ef
Kerns Noel (ASRock) <45953503+DePhoegon@users.noreply.github.com>
2025-08-13 09:01:52 -0600

    Add blockstates and models for new copper/tuff blocks
    Texture Files are Placeholders.
    Introduces blockstate JSONs and model files for various new copper and tuff block variants, including fences, fence gates, slabs, stairs, and walls. Also adds corresponding texture assets and utility scripts to support these blocks in the resource pack.
    
    Took 1 hour 21 minutes

---
+++ WIP _ details/partial_file_gen.bat
@@ -0,0 +1,40 @@
+@echo off
+REM node delbase_json_aid.js [baseblock] [type] [custom/builtin 1/0] [# texture count] [#Model ID(0 MC/2 delbase)] [Texture_top] [texture_side /0 to use first] [texture_inside_side(log)/0 to use first] [texture_inside_top(log)/0 to use first] [texture_inside_bottom(log)/0 to use first] [texture_inside_side2(log)/0 to use first] [texture_inside_top2(log)/0 to use first] [texture_inside_bottom2(log)/0 to use first]
+
+node .\delbase_json_aid.js raw_exposed_copper metal 0 1 2 raw_exposed_block 0 0
+node .\delbase_json_aid.js raw_weathered_copper metal 0 1 2 raw_weathered_block 0 0
+node .\delbase_json_aid.js raw_oxidized_copper metal 0 1 2 raw_oxidized_block 0 0
+node .\delbase_json_aid.js raw_waxed_copper metal 0 1 2 raw_waxed_block 0 0
+node .\delbase_json_aid.js raw_waxed_exposed_copper metal 0 1 2 raw_waxed_exposed_block 0 0
+node .\delbase_json_aid.js raw_waxed_weathered_copper metal 0 1 2 raw_waxed_weathered_block 0 0
+node .\delbase_json_aid.js raw_waxed_oxidized_copper metal 0 1 2 raw_waxed_oxidized_block 0 0
+node .\delbase_json_aid.js waxed_copper metal 0 1 2 waxed_copper_block 0 0
+node .\delbase_json_aid.js waxed_exposed_copper metal 0 1 2 waxed_exposed_copper_block 0 0
+node .\delbase_json_aid.js waxed_weathered_copper metal 0 1 2 waxed_weathered_copper_block 0 0
+node .\delbase_json_aid.js waxed_oxidized_copper metal 0 1 2 waxed_oxidized_copper_block 0 0
+node .\delbase_json_aid.js chiseled_copper metal 0 1 2 chiseled_copper_block 0 0
+node .\delbase_json_aid.js chiseled_exposed_copper metal 0 1 2 chiseled_exposed_copper_block 0 0
+node .\delbase_json_aid.js chiseled_weathered_copper metal 0 1 2 chiseled_weathered_copper_block 0 0
+node .\delbase_json_aid.js chiseled_oxidized_copper metal 0 1 2 chiseled_oxidized_copper_block 0 0
+node .\delbase_json_aid.js chiseled_waxed_copper metal 0 1 2 chiseled_waxed_copper_block 0 0
+node .\delbase_json_aid.js chiseled_waxed_exposed_copper metal 0 1 2 chiseled_waxed_exposed_copper_block 0 0
+node .\delbase_json_aid.js chiseled_waxed_weathered_copper metal 0 1 2 chiseled_waxed_weathered_copper_block 0 0
+node .\delbase_json_aid.js chiseled_waxed_oxidized_copper metal 0 1 2 chiseled_waxed_oxidized_copper_block 0 0
+node .\delbase_json_aid.js copper_bulb metal 0 1 2 copper_bulb_block 0 0
+node .\delbase_json_aid.js exposed_copper_bulb metal 0 1 2 exposed_copper_bulb_block 0 0
+node .\delbase_json_aid.js weathered_copper_bulb metal 0 1 2 weathered_copper_bulb_block 0 0
+node .\delbase_json_aid.js oxidized_copper_bulb metal 0 1 2 oxidized_copper_bulb_block 0 0
+node .\delbase_json_aid.js waxed_copper_bulb metal 0 1 2 waxed_copper_bulb_block 0 0
+node .\delbase_json_aid.js waxed_exposed_copper_bulb metal 0 1 2 waxed_exposed_copper_bulb_block 0 0
+node .\delbase_json_aid.js waxed_weathered_copper_bulb metal 0 1 2 waxed_weathered_copper_bulb_block 0 0
+node .\delbase_json_aid.js waxed_oxidized_copper_bulb metal 0 1 2 waxed_oxidized_copper_bulb_block 0 0
+node .\delbase_json_aid.js cut_copper metal 0 1 2 cut_copper_block 0 0
+node .\delbase_json_aid.js cut_exposed_copper metal 0 1 2 cut_exposed_copper_block 0 0
+node .\delbase_json_aid.js cut_weathered_copper metal 0 1 2 cut_weathered_copper_block 0 0
+node .\delbase_json_aid.js cut_oxidized_copper metal 0 1 2 cut_oxidized_copper_block 0 0
+node .\delbase_json_aid.js cut_waxed_copper metal 0 1 2 cut_waxed_copper_block 0 0
+node .\delbase_json_aid.js cut_waxed_exposed_copper metal 0 1 2 cut_waxed_exposed_copper_block 0 0
+node .\delbase_json_aid.js cut_waxed_weathered_copper metal 0 1 2 cut_waxed_weathered_copper_block 0 0
+node .\delbase_json_aid.js cut_waxed_oxidized_copper metal 0 1 2 cut_waxed_oxidized_copper_block 0 0
+node .\delbase_json_aid.js chiseled_tuff metal 0 1 2 2 chiseled_tuff chiseled_tuff_top 0
+node .\delbase_json_aid.js chiseled_tuff_bricks metal 0 1 2 2 chiseled_tuff_bricks chiseled_tuff_bricks_top 0
